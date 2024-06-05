879153f308a98cbec5e1b418fff333189a3776db
I Wayan Ardika Chandra <iwayanchandra2003@gmail.com>
2024-06-05 20:16:24 +0700

    splash, logo etc..

--- app/src/main/java/com/dicoding/cinesuggest/view/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/cinesuggest/view/login/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.dicoding.cinesuggest.view.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.cinesuggest.R
+import com.dicoding.cinesuggest.databinding.ActivityLoginBinding
+import com.dicoding.cinesuggest.view.signIn.SignInActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInHere.setOnClickListener {
+            navigateToSignIn()
+        }
+    }
+
+    private fun navigateToSignIn() {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/dicoding/cinesuggest/view/signIn/SignInActivity.kt
+++ app/src/main/java/com/dicoding/cinesuggest/view/signIn/SignInActivity.kt
@@ -1,12 +1,27 @@
 package com.dicoding.cinesuggest.view.signIn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.cinesuggest.R
+import com.dicoding.cinesuggest.databinding.ActivitySignInBinding
+import com.dicoding.cinesuggest.view.login.LoginActivity
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginHereText.setOnClickListener {
+            navigateToLogin()
+        }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }

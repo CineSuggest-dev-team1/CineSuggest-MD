94cabe02a3f37a73cde69fd347df3be24951a62c
Ranto Anjasmara Marpaung <rantoanjasmaramarpaung@gmail.com>
2024-06-07 16:50:31 +0700

    Onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    implementation("com.romandanylyk:pageindicatorview:1.0.3")
 }

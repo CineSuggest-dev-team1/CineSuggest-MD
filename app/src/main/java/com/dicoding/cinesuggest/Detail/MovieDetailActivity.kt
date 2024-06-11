ee3227fa08428d8d6f24dc5a1b4278a6be1ca62d
Ranto Anjasmara Marpaung <rantoanjasmaramarpaung@gmail.com>
2024-06-11 09:55:59 +0700

    MovieDetail Layout

---
+++ app/src/main/java/com/dicoding/cinesuggest/Detail/MovieDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.cinesuggest.Detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.cinesuggest.R
+
+class MovieDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}

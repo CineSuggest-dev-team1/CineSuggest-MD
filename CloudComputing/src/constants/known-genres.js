b8af56630cbf9e1000b8c707065633d35487042e
FakhrihanLuthfi <fakhrihanluthfi1@gmail.com>
2024-06-18 15:43:47 +0700

    update

--- CloudComputing/src/constants/known-genres.js
+++
@@ -1,7 +0,0 @@
-/**
- * @prettier
- */
-
-"use strict"
-
-module.exports = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "Thriller", "Tv Movie", "War", "Western"]
